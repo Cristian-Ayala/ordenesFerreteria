770be37009838238e26adbb35d873425ae21bec7
Cristian Ayala <cristianaaron10@gmail.com>
2020-06-22 23:23:10 -0600

    CRUD de productos

--- js/producto.js
+++ js/producto.js
@@ -3,21 +3,23 @@ var vm = new Vue({
     data: {
         productos: [],
         producto: {
-            idMarca: 0,
+            idMarca: null,
             nombreProd: "",
             activoProd: true,
             descripcion: "",
-            idCategoria: 0,
+            idCategoria: null,
             precioUnit: null,
             stockProd: null,
-            upc: null
+            upc: null,
+            nombreMarca: "",
+            nombreCategoria: ""
         },
         displayOption: "",
         searchDisplay: "",
         urlApi: `${ApiRestUrl}producto`,
         marcas: [],
         categorias: [],
-        categoriaSelected: ""
+        add: {}
     },
     methods: {
 
@@ -25,28 +27,11 @@ var vm = new Vue({
         Modifica el registro seleccionado
         */
         edithRegistro() {
-            axios.put(this.urlApi, JSON.stringify(this.marca), {
-                headers: {
-                    'content-type': 'application/json'
-                }
-            }).then(
-                response => {
-                    this.getAll();
-                    console.log(response.status);
-                }
-            ).catch(ex => {
-                console.log(ex)
-            })
-
-        },
-        /*
-        creacion de nuevos registros
-        (no se pueden crear registros vacios)
-         */
-        createRegistro: function () {
-            if (this.marca.nombreMarca.trim() !== "") {
-                console.log(this.marca);
-                axios.post(this.urlApi, JSON.stringify(this.marca), {
+            this.producto.idMarca = this.marcas.filter(mar => mar.nombreMarca === this.producto.nombreMarca)[0].idMarca;
+            this.producto.idCategoria = this.categorias.filter(cat => cat.nombreCat === this.producto.nombreCategoria)[0].idCategoria;
+            if (this.producto.upc.trim() !== "" && this.producto.stockProd > 0 && this.producto.nombreProd.trim() !== "" && this.producto.idMarca > 0 && this.producto.idCategoria > 0) {
+                console.log(this.producto);
+                axios.put(this.urlApi, JSON.stringify(this.producto), {
                     headers: {
                         'content-type': 'application/json'
                     }
@@ -56,6 +41,31 @@ var vm = new Vue({
                 }).catch(ex => {
                     console.log(ex)
                 });
+            } else {
+                console.log("No se pudo editar el producto porque uno de los valores es nulo, indefinido o está vacio");
+            }
+        },
+        /*
+        creacion de nuevos registros
+        (no se pueden crear registros vacios)
+         */
+        createRegistro: function () {
+            this.producto.idMarca = this.marcas.filter(mar => mar.nombreMarca === this.producto.nombreMarca)[0].idMarca;
+            this.producto.idCategoria = this.categorias.filter(cat => cat.nombreCat === this.producto.nombreCategoria)[0].idCategoria;
+            if (this.producto.upc.trim() !== "" && this.producto.stockProd > 0 && this.producto.nombreProd.trim() !== "" && this.producto.idMarca > 0 && this.producto.idCategoria > 0) {
+                console.log(this.producto);
+                axios.post(this.urlApi, JSON.stringify(this.producto), {
+                    headers: {
+                        'content-type': 'application/json'
+                    }
+                }).then(response => {
+                    console.log(response.status);
+                    this.getAll();
+                }).catch(ex => {
+                    console.log(ex)
+                });
+            } else {
+                console.log("No se pudo registrar el producto porque uno de los valores es nulo, indefinido o está vacio");
             }
 
         },
@@ -64,8 +74,7 @@ var vm = new Vue({
         eliman registros, correspondiente al id seleccionado
          */
         removeRegistro: function () {
-            this.displayOption = 'eliminar';
-            axios.put(this.urlApi + "/remove/" + this.marca.idMarca).then(
+            axios.put(this.urlApi + "/remove/" + this.producto.upc).then(
                 response => {
                     this.getAll();
                     console.log(response.status)
@@ -84,6 +93,11 @@ var vm = new Vue({
                 response => {
                     this.productos = response.data
                     this.productos = this.productos.filter(prod => prod.activoProd === true);
+                    this.productos.map(prod => {
+                        prod.nombreCategoria = this.categorias.filter(cat => cat.idCategoria === prod.idCategoria)[0].nombreCat;
+                        prod.nombreMarca = this.marcas.filter(mar => mar.idMarca === prod.idMarca)[0].nombreMarca;
+                    });
+
                 }
             ).catch(ex => {
                 console.log(ex)
@@ -94,27 +108,42 @@ var vm = new Vue({
         limpiando valores de la marca previamente seleccionada
          */
         clearData() {
-            this.marca = {
-                idMarca: 0,
+            this.producto = {
+                idMarca: null,
                 nombreProd: "",
                 activoProd: true,
                 descripcion: "",
-                idCategoria: 0,
+                idCategoria: null,
                 precioUnit: null,
                 stockProd: null,
-                upc: null
+                upc: null,
+                nombreMarca: "",
+                nombreCategoria: ""
             }
         },
-        getMarcaSelected(mar) {
-            this.marca = mar;
+        getProductoSelected(prod) {
+            this.producto = prod;
+            // this.producto.idMarca = this.marcas.filter(mar => mar.idMarca === this.producto.idMarca)[0].nombreMarca;
+            // console.log(this.marcas.filter(mar => mar.idMarca === this.producto.idMarca)[0].nombreMarca);
+            // var idMar = typeof this.marcas !== 'undefined'?this.marcas.filter(mar => mar.idMarca === this.producto.idMarca)[0].nombreMarca:'';
+            // this.producto.idMarca = idMar;
         },
         filtro(valor) {
             if (this.searchDisplay === "") return true;
-            // idMarca: 0,
-            // idCategoria: 0,
-            let array = (this.productos[valor].nombreProd + this.productos[valor].descripcion+this.productos[valor].precioUnit+this.productos[valor].stockProd+this.productos[valor].upc).toUpperCase();
+            let array = (this.marcas.filter(mar => mar.idMarca === this.productos[valor].idMarca)[0].nombreMarca + this.categorias.filter(cat => cat.idCategoria === this.productos[valor].idCategoria)[0].nombreCat + this.productos[valor].nombreProd + this.productos[valor].descripcion + this.productos[valor].precioUnit + this.productos[valor].stockProd + this.productos[valor].upc).toUpperCase();
             return array.indexOf(this.searchDisplay.toUpperCase()) >= 0;
         },
+        showEditing: function (input) {
+            // Get the value from the input
+            var value = input.value;
+            // Get the matching `option` element from the `datalist` (which is
+            // available via `input.list`)
+            var option = Array.prototype.find.call(input.list.options, function (option) {
+                return option.value === value;
+            });
+            // Get its `data-id` attribute value
+            console.log(option.getAttribute("data-id"));
+        }
     },
     /*
     hook para inicializar los valores de la tabla
